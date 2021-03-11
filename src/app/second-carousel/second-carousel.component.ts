f63f205163ddae14b5fc74f61334b62debfbb046
RomanRolginSky <roman.rolgin@skyeng.ru>
2021-03-11 19:34:32 +0300

    2 files modified

--- src/app/second-carousel/second-carousel.component.ts
+++ src/app/second-carousel/second-carousel.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NguCarouselConfig } from "@ngu/carousel";
 
 @Component({
-  selector: 'app-second-carousel',
-  templateUrl: './second-carousel.component.html',
-  styleUrls: ['./second-carousel.component.css']
+  selector: "app-second-carousel",
+  templateUrl: "./second-carousel.component.html",
+  styleUrls: ["./second-carousel.component.css"]
 })
 export class SecondCarouselComponent implements OnInit {
-
-  constructor() { }
+  imgags = [
+    "assets/bg.jpg",
+    "assets/car.png",
+    "assets/canberra.jpg",
+    "assets/holi.jpg"
+  ];
+  public carouselTileItems: Array<any> = [0, 1, 2, 3, 4, 5];
+  public carouselTiles = {
+    0: [],
+    1: [],
+    2: [],
+    3: [],
+    4: [],
+    5: []
+  };
+  public carouselTile: NguCarouselConfig = {
+    grid: { xs: 1, sm: 1, md: 3, lg: 3, xl: 3, all: 0 },
+    slide: 3,
+    speed: 250,
+    point: {
+      visible: true
+    },
+    load: 2,
+    velocity: 0,
+    touch: true,
+    easing: "cubic-bezier(0, 0, 0.2, 1)"
+  };
+  constructor() {}
 
   ngOnInit() {
+    this.carouselTileItems.forEach(el => {
+      this.carouselTileLoad(el);
+    });
   }
 
+  public carouselTileLoad(j) {
+    // console.log(this.carouselTiles[j]);
+    const len = this.carouselTiles[j].length;
+    if (len <= 30) {
+      for (let i = len; i < len + 15; i++) {
+        this.carouselTiles[j].push(
+          this.imgags[Math.floor(Math.random() * this.imgags.length)]
+        );
+      }
+    }
+  }
 }
