fa04f17358ecd2e3605aafca69f0b1741f5354df
RomanRolginSky <roman.rolgin@skyeng.ru>
2021-03-11 19:16:59 +0300

    1 file modified, 2 added

--- src/app/first-carousel/first-carousel.component.ts
+++ src/app/first-carousel/first-carousel.component.ts
@@ -1,17 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-first-carousel',
-  templateUrl: './first-carousel.component.html',
-  styleUrls: ['./first-carousel.component.css']
+  selector: "app-first-carousel",
+  templateUrl: "./first-carousel.component.html",
+  styleUrls: ["./first-carousel.component.css"]
 })
-export class FirstCarouselComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  items = new Array(20).fill(undefined).map((value, index) => ({index, name: `Item ${index}`}));
-
+export class FirstCarouselComponent {
+  items = new Array(20)
+    .fill(undefined)
+    .map((value, index) => ({ index, name: `Item ${index}` }));
 }
