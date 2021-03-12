05be1cbde3075474cb6c8934d603cf192eeef5ce
Roman Rolgin <roman.rolgin@netcracker.com>
2021-03-12 19:16:52 +0300

    carousels

--- src/app/third-carousel/third-carousel.component.ts
+++ src/app/third-carousel/third-carousel.component.ts
@@ -1,16 +1,17 @@
-import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { fromEvent } from 'rxjs';
-import { throttleTime } from 'rxjs/operators';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+import { from } from 'rxjs';
+import { map } from 'rxjs/operators';
+// import { fromEvent } from 'rxjs';
+// import { throttleTime } from 'rxjs/operators';
 // import * as Glider from 'glider-js';
+// import * Glider from 'glider-js';
 
-declare const Glider: any;
-// tslint:disable-next-line:no-namespace
-declare namespace Glider {}
+type Gliderjs = typeof import('glider-js');
 
 @Component({
   selector: 'app-third-carousel',
   templateUrl: './third-carousel.component.html',
-  styleUrls: ['./third-carousel.component.css']
+  styleUrls: ['./third-carousel.component.scss']
 })
 export class ThirdCarouselComponent implements AfterViewInit, OnDestroy {
 
@@ -22,17 +23,34 @@ export class ThirdCarouselComponent implements AfterViewInit, OnDestroy {
   private glider!: any;
 
   ngAfterViewInit(): void {
-    this.init();
+    from(import(/* webpackChunkName: 'glider-js' */ 'glider-js'))
+      .pipe(
+        // @ts-ignore
+        map(glider => glider.default))
+      .subscribe((gliderInit) => {
+        this.init(gliderInit);
+      });
+    // import(/*webpack-chunk*/'glider-js').then(module => module.default).then(gliderInit => {
+    //   this.init(gliderInit);
+    // });
+
+    // this.init(gliderInit);
     // fromEvent(this.carousel.nativeElement, 'scroll').pipe(
     //   throttleTime(300, undefined, { leading: true, trailing: true }),
     // ).subscribe(() => this.glider && this.glider.updateControls());
   }
 
-  private init(): void {
+  ngOnDestroy(): void {
+    if (this.glider) {
+      this.glider.destroy();
+    }
+  }
+
+  private init(gliderInit: any): void {
     if (this.glider) {
       return;
     }
-    this.glider = new Glider(this.carousel.nativeElement, {
+    this.glider = new gliderInit(this.carousel.nativeElement, {
       // scrollLock: true,
       slidesToShow: 'auto',
       slidesToScroll: 3,
@@ -44,11 +62,5 @@ export class ThirdCarouselComponent implements AfterViewInit, OnDestroy {
         next: '.glider-next',
       }
     });
-  }
-
-  ngOnDestroy(): void {
-    if (this.glider) {
-      this.glider.destroy();
-    }
   }
 }
